1ddd9d011b9c23c2bf80747a3bda231ecb6a88c3
losthj <thislost@gmail.com>
2019-04-19 11:00:50 +0800

    update context

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NameCard from './components/namecard';
 import ThemeContext from './theme-context'
+import ThemendBar from './components/ThemedBar'
 
 const tags=['恐龙','足球小子'];
 
-const thems ={
-  ligh:{
+const themes ={
+  light:{
     classname:'btn btn-primary',
     bgColor:'#eeeeee',
     color:'#000'
@@ -19,25 +20,36 @@ const thems ={
 }
 
 
+
 class App extends Component {
   constructor(props){
     super(props)
     this.state={
-
+      theme:'light'
     }
-
+this.changeTheme = this.changeTheme.bind(this);
   }
 
-
+  changeTheme(theme){
+    this.setState({
+      theme,
+    })
+  }
 render() {
     return (
+      <ThemeContext.Provider value={themes[this.state.theme]}>
       <div>
         <NameCard name="huangjian" number={123112312} isHuman tags={tags} />
 
-        <a href="#theme-switcher" className="btn btn-light">浅色主题</a>
-        <a href="#theme-switcher" className="btn btn-secondary">深色主题</a>
-
+        <a href="#theme-switcher" className="btn btn-light"
+        onClick={() => {this.changeTheme('light')}}
+        >浅色主题</a>
+        <a href="#theme-switcher" className="btn btn-secondary"
+        onClick={() => {this.changeTheme('dark')}}
+        >深色主题</a>
+      <ThemendBar></ThemendBar>
       </div>
+      </ThemeContext.Provider>
     );
   }
 }
---
+++ src/components/ThemedBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ThemeContext from '../theme-context';
+
+const ThemedBar =() =>{
+    return(
+        <ThemeContext.Consumer>
+            {
+                theme=>{
+                    console.log(theme)
+
+                    return(
+                        <div className="alert mt-5" 
+                        style={{backgroundColor:theme.bgColor,color:theme.color}}>
+                            样式区域
+                            <button className={theme.classname}>样式按钮</button>
+                        </div>
+                    )
+                }
+            }
+        </ThemeContext.Consumer>
+    )
+}
+export default ThemedBar;
